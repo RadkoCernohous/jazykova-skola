70ec8bc44ab5e5ea228f308c6bd6a8a40cc0a843
RadkoCernohous <111200624+RadkoCernohous@users.noreply.github.com>
2023-03-09 21:41:22 +0100

    Update jsEdit.js

--- js/jsEdit.js
+++ js/jsEdit.js
@@ -60,7 +60,7 @@ sipka.style.cssText = `
 
 document.body.appendChild(sipka);
 window.addEventListener('scroll', function () {
-  if ((window.scrollY > (window.innerHeight / 3)) && window.innerWidth > 1300) {
+  if ((window.scrollY > (window.innerHeight / 3)) && window.innerWidth > 2000) {
     sipka.style.display = 'block';
   } else {
     sipka.style.display = 'none';
